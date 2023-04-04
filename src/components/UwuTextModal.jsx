78be3c269a0151e064658ba72a215b4d0e5a42c7
Olivier Reynard <olivier.reynard17@gmail.com>
2023-04-05 00:07:30 +0200

    can now copy the uwu text with a single click

--- src/components/UwuTextModal.jsx
+++ src/components/UwuTextModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
     Text,
     Button,
@@ -9,14 +9,28 @@ import {
     ModalFooter,
     ModalBody,
     ModalCloseButton,
-    CircularProgress
+    CircularProgress,
+    Flex,
+    useClipboard
 } from '@chakra-ui/react'
+import {HiClipboardCopy} from 'react-icons/hi'
 
 const UwuTextModal = ( {uwuText, loading, isOpen, closeModal}) => {
+
+    //Chakra custom hook that handles copying content to clipboard
+    const { onCopy, value, setValue, hasCopied } = useClipboard("");
+
+    const copyToClipboard = () => {
+        setValue(uwuText)
+        //Custom JS, useClipboard hook seems to bug on its own
+        navigator.clipboard.writeText(uwuText);
+        onCopy()
+    }
+
   return (
     <Modal isOpen={isOpen} onClose={closeModal}>
         <ModalOverlay/>
-        <ModalContent>
+        <ModalContent bg='pink.300'>
             <ModalHeader>
                 Text uwu'd :
             </ModalHeader>
@@ -31,9 +45,23 @@ const UwuTextModal = ( {uwuText, loading, isOpen, closeModal}) => {
                 )}
             </ModalBody>
             <ModalFooter>
-                <Button colorScheme='pink' mr={3} onClick={closeModal}>
-                    Close
-                </Button>
+                <Flex>
+                {loading ? (
+                    <></>
+                ) : (
+                    <Button 
+                    colorScheme='pink' 
+                    mr={3} 
+                    rightIcon={ <HiClipboardCopy/> } 
+                    onClick={copyToClipboard}
+                    >
+                       {hasCopied ? "Copied!" : "Copy"}
+                    </Button>
+                )}
+                    <Button colorScheme='pink' mr={3} onClick={closeModal}>
+                        Close
+                    </Button>
+                </Flex>
             </ModalFooter>
         </ModalContent>
     </Modal>
